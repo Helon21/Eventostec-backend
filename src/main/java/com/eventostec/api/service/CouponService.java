00540e051c314530e0a7f094692562e60e8ce414
Helon21 <helonbentes51@hotmail.com>
2025-01-23 18:26:02 -0300

    feat: create coupon CRUD

---
+++ src/main/java/com/eventostec/api/service/CouponService.java
@@ -0,0 +1,39 @@
+package com.eventostec.api.service;
+
+import com.eventostec.api.domain.coupon.Coupon;
+import com.eventostec.api.domain.coupon.CouponRequestDTO;
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.repository.CouponRepository;
+import com.eventostec.api.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class CouponService {
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO data) {
+        Event event = eventRepository.findById(eventId).orElseThrow();
+
+        Coupon coupon = new Coupon();
+        coupon.setCode(data.code());
+        coupon.setValid(new Date(data.valid()));
+        coupon.setDiscount(data.discount());
+        coupon.setEvent(event);
+
+        return couponRepository.save(coupon);
+    }
+
+    public List<Coupon> consultCoupons(UUID eventId, Date currentDate) {
+        return couponRepository.findByEventIdAndValidAfter(eventId, currentDate);
+    }
+}
